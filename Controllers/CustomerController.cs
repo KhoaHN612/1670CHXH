13593364eb010920b828665048e144ea0f2a8455
NguyenThanhQuyGCC210142 <quyntgcc210142@fpt.edu.vn>
2023-10-25 08:31:03 +0700

    Add customer index

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ASMProject.Models;
+
+namespace ASMProject.Controllers;
+
+public class CustomerController : Controller
+{
+    private readonly ILogger<CustomerController> _logger;
+
+    public CustomerController(ILogger<CustomerController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
