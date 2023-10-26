a7a7aecbddd8e0e9bbbcb333b66e7e76e106a615
NguyenThanhQuyGCC210142 <quyntgcc210142@fpt.edu.vn>
2023-10-26 11:06:57 +0700

    Config some layout of pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,21 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Products()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
