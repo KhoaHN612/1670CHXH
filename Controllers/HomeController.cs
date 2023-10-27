2c85bc163fdc704b6a94dd2a576a96d15088f145
NguyenThanhQuyGCC210142 <quyntgcc210142@fpt.edu.vn>
2023-10-27 09:52:52 +0700

    Complete pages for guest

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
     public IActionResult Login()
     {
         return View();
