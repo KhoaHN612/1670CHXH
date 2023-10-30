b1e522432d14070af3cdaf929e3c2a955cb8ef8b
NguyenThanhQuyGCC210142 <quyntgcc210142@fpt.edu.vn>
2023-10-31 00:50:48 +0700

    config the show of Storeowner index

--- Controllers/StoreownerController.cs
+++ Controllers/StoreownerController.cs
@@ -7,16 +7,24 @@ namespace ASMProject.Controllers;
 
 public class StoreownerController : Controller
 {
+
+    private readonly ILogger<HomeController> _logger;
     private readonly Db1670asmContext _context;
 
-    public StoreownerController(Db1670asmContext context)
+    public StoreownerController(ILogger<HomeController> logger, Db1670asmContext context)
     {
+        _logger = logger;
         _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var books = await _context.Books.ToListAsync();
+        if (books == null)
+        {
+            return NotFound();
+        }
+        return View(books);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
